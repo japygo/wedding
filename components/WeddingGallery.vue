18b33ce6e3ac44eafac9c634a218810a231d0c39
japygo <hyunnjinn@gmail.com>
2023-03-08 20:39:20 +0900

    Update 사진 추가

--- components/WeddingGallery.vue
+++ components/WeddingGallery.vue
@@ -19,12 +19,22 @@ export default {
   data() {
     return {
       images: [
-        require('@/static/imgs/5.jpg'),
-        require('@/static/imgs/3.jpg'),
-        require('@/static/imgs/4.jpg'),
-        require('@/static/imgs/7.jpg'),
-        require('@/static/imgs/2.jpg'),
-        require('@/static/imgs/1.jpg'),
+        // require('@/static/imgs/5.jpg'),
+        // require('@/static/imgs/3.jpg'),
+        // require('@/static/imgs/4.jpg'),
+        // require('@/static/imgs/7.jpg'),
+        // require('@/static/imgs/2.jpg'),
+        // require('@/static/imgs/1.jpg'),
+        require('@/static/imgs/11.jpg'),
+        require('@/static/imgs/15.jpg'),
+        require('@/static/imgs/16.jpg'),
+        require('@/static/imgs/17.jpg'),
+        require('@/static/imgs/8.jpg'),
+        require('@/static/imgs/9.jpg'),
+        require('@/static/imgs/10.jpg'),
+        require('@/static/imgs/12.jpg'),
+        require('@/static/imgs/13.jpg'),
+        require('@/static/imgs/14.jpg'),
       ],
     }
   }
